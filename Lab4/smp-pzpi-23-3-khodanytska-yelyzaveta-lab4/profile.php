6c7a32044b6b14ad4c9a9b22bbd5cc7f4e4a64b2
NureKhodanytskaYelyzaveta <yelyzaveta.khodanytska@nure.ua>
2025-05-20 15:29:38 +0300

    Add files via upload

---
+++ Lab4/smp-pzpi-23-3-khodanytska-yelyzaveta-lab4/profile.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'first_name' => 'лі',
+  'last_name' => 'лі',
+  'birth_date' => '2000-04-20',
+  'bio' => 'якась інформація 023189крта3цузщташсдцжьФСТ',
+  'photo' => NULL,
+);
